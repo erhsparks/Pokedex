78c0052c127cab8be7d706cb015de85628fb258d
Elizabeth Sparks <erhsparks@gmail.com>
2016-10-27 15:53:27 -0700

    add click listener to each pokemon li in pokemon index: will render detail for that pokemon

--- frontend/components/pokemon/pokemon_index.jsx
+++ frontend/components/pokemon/pokemon_index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PokemonIndexItem from './pokemon_index_item';
 
 export default ({ pokemon }) => (
-  <ul>
+  <ul className='pokemon-index'>
     {pokemon.map(poke => (
       <PokemonIndexItem key={poke.id} pokemon={poke} />
     ))}
--- frontend/components/pokemon/pokemon_index_item.jsx
+++ frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 
-export default ({ pokemon }) => (
-  <li>
-    <img src={pokemon.image_url}
-      style={{width: 20, height: 20}}
-      alt={`Image of ${pokemon.name}`} />
-    {pokemon.name}
-  </li>
-);
+const PokemonIndexItem = ({ router, pokemon }) => {
+  const handleClick = url => e => router.push(url);
+
+  return (
+    <li className='pokemon-index-item'
+        onClick={handleClick(`/pokemon/${pokemon.id}`)}>
+      <img src={pokemon.image_url}
+        style={{width: 20, height: 20}}
+        alt={`Image of ${pokemon.name}`} />
+      {pokemon.name}
+    </li>
+  );
+};
+
+export default withRouter(PokemonIndexItem);
