e832b6cb3b824908a0215ad97b00b0dde359c948
Elizabeth Sparks <erhsparks@gmail.com>
2016-10-27 15:12:44 -0700

    use React Router to fetch all pokemon from db and render pokemon index when app's root component is rendered

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, hashHistory } from 'react-router';
 import PokemonIndexContainer from './pokemon/pokemon_index_container';
+import { requestAllPokemon } from '../actions/pokemon_actions';
 
-const Root = ({ store }) => (
-  <Provider store={store}>
-    <PokemonIndexContainer />
-  </Provider>
-);
+const Root = ({ store }) => {
+  const requestOnEnter = () => {
+    store.dispatch(requestAllPokemon());
+  };
+
+  return (
+    <Provider store={store}>
+      <Router history={hashHistory}>
+        <Route path='/'
+               onEnter={requestOnEnter}
+               component={PokemonIndexContainer} />
+      </Router>
+    </Provider>
+  );
+};
 
 export default Root;
